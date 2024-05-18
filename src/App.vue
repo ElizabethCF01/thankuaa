a31c64af253940c33a0b7907fa2378beda0d0601
ElizabethCF01 <elizabethcalzadillafernandez@gmail.com>
2024-05-18 02:12:13 -0400

    thanks 😁

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Thanks from './components/Thanks.vue'
+</script>
+
+<template>
+  <main>
+    <Thanks />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Thanks.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { Typed } from "@duskmoon/vue3-typed-js";
+import type { TypedOptions } from "@duskmoon/vue3-typed-js";
+
+const options: TypedOptions = {
+  strings: ["Maybe you're just doing your job", "But I really appreciate your help", "I'm glad I met you 😁", "PS: Please tell me what was the area of ​​the crown-shaped figure 🥲"],
+  loop: true,
+  typeSpeed: 100,
+};
+</script>
+
+<template>
+  <div class="terminal-loader">
+    <div class="terminal-header">
+      <div class="terminal-title">Hi, Alex</div>
+      <div class="terminal-controls">
+        <div class="control close"></div>
+        <div class="control minimize"></div>
+        <div class="control maximize"></div>
+      </div>
+    </div>
+    <Typed :options="options">
+      <span class="typing"></span>
+    </Typed>
+  </div>
+</template>
+<style scoped>
+.terminal-loader {
+  border: 0.1em solid #333;
+  background-color: #1a1a1a;
+  color: #0f0;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 1em;
+  padding: 2.5em 1em;
+  width: 29em;
+  height: 10em;
+  margin: 100px auto;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  position: relative;
+  overflow: hidden;
+  box-sizing: border-box;
+}
+
+.terminal-header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 1.5em;
+  background-color: #333;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+  padding: 0 0.4em;
+  box-sizing: border-box;
+}
+
+.terminal-controls {
+  float: right;
+}
+
+.control {
+  display: inline-block;
+  width: 0.6em;
+  height: 0.6em;
+  margin-left: 0.4em;
+  border-radius: 50%;
+  background-color: #777;
+}
+
+.control.close {
+  background-color: #e33;
+}
+
+.control.minimize {
+  background-color: #ee0;
+}
+
+.control.maximize {
+  background-color: #0b0;
+}
+
+.terminal-title {
+  float: left;
+  line-height: 1.5em;
+  color: #eee;
+}
+
+.text {
+  display: inline-block;
+  white-space: nowrap;
+  overflow: hidden;
+  border-right: 0.2em solid green;
+  /* Cursor */
+  animation: typeAndDelete 4s steps(11) infinite,
+    blinkCursor 0.5s step-end infinite alternate;
+  margin-top: 1.5em;
+}
+</style>
